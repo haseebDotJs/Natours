246a30bd5e8adeb807618c185b8120cab02c0f8c
haseebDotJs <haseeb.khalid03452800681@gmail.com>
2021-09-06 20:36:10 +0500

    converted class names to bem arcitecture

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,15 +13,15 @@ const Header = () => {
 
     return (
         <header className="header">
-            <div className="logo-box">
-                <img className="logo" src={whiteLogo} alt="logo" />
+            <div className="header__logo-box">
+                <img className="header__logo" src={whiteLogo} alt="logo" />
             </div>
-            <div className="text-box">
+            <div className="header__text-box">
                 <h1 className="heading-primary">
-                    <span className="heading-primary-main" >outdoors</span>
-                    <span className="heading-primary-sub">is where life happens</span>
+                    <span className="heading-primary--main" >outdoors</span>
+                    <span className="heading-primary--sub">is where life happens</span>
                 </h1>
-                <a href="#" className="btn btn-white btn-animated">discover tours</a>
+                <a href="#" className="btn btn--white btn--animated">discover tours</a>
             </div>
         </header>
     )
